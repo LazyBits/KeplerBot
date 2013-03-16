7e2d13757d4b1653b3f4d523a632ed2bf8c6a6af
Nick Van den Putte <crazyputje@gmail.com>
2013-03-16 21:13:47 +0100

    Color codes

---
+++ Keplerbot/src/net/keplergaming/keplerbot/utils/ColorCodes.java
@@ -0,0 +1,21 @@
+package net.keplergaming.keplerbot.utils;
+
+public class ColorCodes {
+
+	public static String BLUE = "#0000FF";
+	public static String CORAL = "#FF7F50";
+	public static String DODGERBLUE = "#1E90FF";
+	public static String SPRINGGREEN = "#00FF7F";
+	public static String YELLOWGREEN = "#9ACD32";
+	public static String GREEN = "#008000";
+	public static String ORANGERED = "#FF4500";
+	public static String RED = "#FF0000";
+	public static String GOLDENROD = "#DAA520";
+	public static String HOTPINK = "#FF69B4";
+	public static String CADETBLUE = "#5F9EA0";
+	public static String SEAGREEN = "#2E8B57";
+	public static String CHOCOLATE = "#D2691E";
+	public static String BLUEVIOLET = "#8A2BE2";
+	public static String FIREBRICK = "#B22222";
+
+}
