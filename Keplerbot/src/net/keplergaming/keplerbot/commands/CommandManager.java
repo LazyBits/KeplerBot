a1a36ec2e30064828414b7d05da5d8ee18294146
Nick Van den Putte <crazyputje@gmail.com>
2013-03-17 21:15:41 +0100

    The command registerer works now :P
    I know it's a bit too much work for what it does ;)

--- Keplerbot/src/net/keplergaming/keplerbot/commands/CommandManager.java
+++ Keplerbot/src/net/keplergaming/keplerbot/commands/CommandManager.java
@@ -1,11 +1,15 @@
 package net.keplergaming.keplerbot.commands;
 
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import net.keplergaming.keplerbot.KeplerBot;
 import net.keplergaming.keplerbot.KeplerBotWrapper;
@@ -70,26 +74,69 @@ public class CommandManager extends ListenerAdapter<KeplerBot>{
 		}
 	}
 
-	public void registerDefaultCommands() {
-		URL root = Thread.currentThread().getContextClassLoader().getResource("net/keplergaming/keplerbot/commands/defaults");
+	private void registerDefaultCommands() {
+		String pkgname = "net.keplergaming.keplerbot.commands.defaults";
 
-		File[] files = new File(root.getFile()).listFiles(new FilenameFilter() {
-		    public boolean accept(File dir, String name) {
-		        return name.endsWith(".class");
-		    }
-		});
+		File directory = null;
+		String fullPath;
+		String relPath = pkgname.replace('.', '/');
+		logger.fine("ClassDiscovery: Package: " + pkgname + " becomes Path:" + relPath);
+		URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
+		logger.fine("ClassDiscovery: Resource = " + resource);
+		if (resource == null) {
+			logger.error("No resource for " + relPath);
+			return;
+		}
+		fullPath = resource.getFile();
+		logger.fine("ClassDiscovery: FullPath = " + resource);
 
-		for (File file : files) {
-		    String className = file.getName().replaceAll(".class$", "");
-		    Class<?> cls;
+		try {
+			directory = new File(resource.toURI());
+		} catch (URISyntaxException e) {
+			logger.error("Error when registering default commands", e);
+		} catch (IllegalArgumentException e) {
+			directory = null;
+		}
+		logger.fine("ClassDiscovery: Directory = " + directory);
+
+		if (directory != null && directory.exists()) {
+			String[] files = directory.list();
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].endsWith(".class")) {
+					String className = pkgname + '.' + files[i].substring(0, files[i].length() - 6);
+					logger.fine("ClassDiscovery: className = " + className);
+					try {
+						if (ICommand.class.isAssignableFrom(Class.forName(className))) {
+							registerCommand((ICommand) Class.forName(className).newInstance());
+						}
+					} catch (Exception e) {
+						logger.error("Error when registering default commands", e);
+					}
+				}
+			}
+		} else {
 			try {
-				cls = Class.forName("net.keplergaming.keplerbot.commands.defaults." + className);
-			    if (ICommand.class.isAssignableFrom(cls)) {
-					registerCommand((ICommand) cls.newInstance());
-
-			    }
-			} catch (Exception e) {
-				logger.warning("Error when registering default commands", e);
+				String jarPath = fullPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
+				JarFile jarFile = new JarFile(jarPath);
+				Enumeration<JarEntry> entries = jarFile.entries();
+				while (entries.hasMoreElements()) {
+					JarEntry entry = entries.nextElement();
+					String entryName = entry.getName();
+					if (entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
+						logger.fine("ClassDiscovery: JarEntry: " + entryName);
+						String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+						logger.fine("ClassDiscovery: className = " + className);
+						try {
+							if (ICommand.class.isAssignableFrom(Class.forName(className))) {
+								registerCommand((ICommand) Class.forName(className).newInstance());
+							}
+						} catch (Exception e) {
+							logger.error("Error when registering default commands", e);
+						}
+					}
+				}
+			} catch (IOException e) {
+				logger.error(pkgname + " (" + directory + ") does not appear to be a valid package", e);
 			}
 		}
 	}
